64f410be5d91d2f222a67a2b70cb1141e30ac170
saantosiras <90818783+saantosiras@users.noreply.github.com>
2025-12-17 19:39:39 +0530

    Create main.tf

---
+++ environments/dev/main.tf
@@ -0,0 +1,16 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "dev_rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+module "network" {
+  source              = "../../modules/network"
+  resource_group_name = azurerm_resource_group.dev_rg.name
+  location            = var.location
+  hub_vnet_cidr       = var.hub_vnet_cidr
+  spoke_vnet_cidr     = var.spoke_vnet_cidr
+}
