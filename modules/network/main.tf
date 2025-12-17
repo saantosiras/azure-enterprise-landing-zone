ecc63cce15608437e6c21703c36d8ef29a6b444d
saantosiras <90818783+saantosiras@users.noreply.github.com>
2025-12-17 19:35:00 +0530

    Update main.tf

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -10,3 +10,17 @@ terraform {
 provider "azurerm" {
   features {}
 }
+resource "azurerm_virtual_network" "hub_vnet" {
+  name                = "vnet-hub"
+  address_space       = [var.hub_vnet_cidr]
+  location            = var.location
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_virtual_network" "spoke_vnet" {
+  name                = "vnet-spoke"
+  address_space       = [var.spoke_vnet_cidr]
+  location            = var.location
+  resource_group_name = var.resource_group_name
+}
+
