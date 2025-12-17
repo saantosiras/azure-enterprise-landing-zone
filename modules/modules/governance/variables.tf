505933c8612cec902d497e11632a1347798428ad
saantosiras <90818783+saantosiras@users.noreply.github.com>
2025-12-17 19:52:54 +0530

    Delete modules/modules/governance directory

--- modules/modules/governance/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "resource_group_id" {
-  description = "Resource Group ID for policy assignment"
-  type        = string
-}
-
-variable "allowed_locations" {
-  description = "List of allowed Azure regions"
-  type        = list(string)
-}
